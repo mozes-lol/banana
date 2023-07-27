987208ef804010c8a2ba771e6492c491cf25acef
mozes-lol <johnmarkgarcia164@gmail.com>
2023-07-27 18:31:58 +0800

    Re-implement the timer feature

--- levels/level_controller.gd
+++ levels/level_controller.gd
@@ -7,7 +7,7 @@ var currentVehicle
 func _ready():
 	var vehicleSpawn = load(vehiclePathnameList[0]).instantiate() # change the index later
 	add_child(vehicleSpawn)
-	#get_node("start_game_on_timer").initiateStartTimer()
+	get_node("start_game_on_timer").initiateStartTimer()
 	pass
 	
 	
--- levels/start_game_on_timer.gd
+++ levels/start_game_on_timer.gd
@@ -1,9 +1,11 @@
 extends Node
 
+@onready var parent = get_node("/root/level_test_3d/level_controller")
+
 func initiateStartTimer():
 	get_node("start_timer").start()
 	print("Get ready...")
 
 func _on_start_timer_timeout():
-	get_node("/root/level_test_3d/level_controller").currentVehicle.move_status = "auto"
+	parent.currentVehicle.move_status = "auto"
 	print("Go!")
