c1a702211010a6759ffbaee9af74cf7412414d6e
mozes-lol <johnmarkgarcia164@gmail.com>
2023-08-05 13:12:51 +0800

    add arrow guides

---
+++ objects/arrow_guide.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export var offset = Vector3()
+
+@onready var levelController = get_node("/root/level_test_3d/level_controller")
+@onready var destination = get_node("/root/level_test_3d/destination")
+
+func _process(delta: float) -> void:
+	print(destination)
+	global_position = levelController.currentVehicle.global_position + offset
+	look_at(levelController.currentVehicle.destination_position)
